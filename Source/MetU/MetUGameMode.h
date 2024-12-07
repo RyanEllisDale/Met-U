63e058c0ae240199ee195f4291d4781d5b3df199
RyanEllisDale <ryanellisDale@gmail.com>
2024-12-07 11:43:11 +0000

    Starting Project
    C++ First Person Template,
    No Starter Content
    Desktop Development
    Maximum Quality Settings

---
+++ Source/MetU/MetUGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetUGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMetUGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMetUGameMode();
+};
+
+
+
